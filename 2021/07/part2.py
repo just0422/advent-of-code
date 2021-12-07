685a7a5f44e410e0f0edea1c52a2af757b64d4e6
Justin Maldonado <just0422@gmail.com>
2021-12-07 15:43:30 -0500

    AOC Day 7

---
+++ 2021/07/part2.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+with open("input.txt") as positions:
+    pos_dict = defaultdict(int)
+
+    for pos in positions.readline().rstrip().split(','):
+        pos_dict[int(pos)]+=1
+
+    total_distances = []
+    for i in range(max(pos_dict.keys()) - min(pos_dict.keys())):
+        sub_total_distance = 0
+        for k,v in pos_dict.items():
+            sub_total_distance += sum(range(abs(k-i) + 1)) * v
+        total_distances.append(sub_total_distance)
+
+    print(min(total_distances))
