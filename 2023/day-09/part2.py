eee1133a19d4add6aa66090c9233e40b7c196436
Justin Maldonado <justin.maldonado@mlb.com>
2023-12-09 07:38:31 -0500

    Day 9

---
+++ 2023/day-09/part2.py
@@ -0,0 +1,25 @@
+import sys
+from collections import defaultdict
+
+def check_zeros(seq):
+    for num in seq:
+        if num != 0:
+            return False
+    return True
+
+def difference_down(seq):
+    if check_zeros(seq):
+        return 0
+
+    next_diff = difference_down([seq[x] - seq[x-1] for x in range(1, len(seq))])
+
+    return seq[0] - next_diff
+
+with open(sys.argv[1]) as sequence_file:
+    sequence_sum = 0
+    while (line := sequence_file.readline().rstrip()):
+        sequence = [int(x) for x in line.split(' ')]
+
+        sequence_sum += difference_down(sequence)
+
+    print(sequence_sum)
